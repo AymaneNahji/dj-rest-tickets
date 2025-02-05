30fe5510cd93ee096b66bcbd436732a2e385bb64
Aymane Nahji <aymanenahjidev@gmail.com>
2025-02-05 15:40:56 +0100

    discard last commit

--- dj_rest_tickets/utils.py
+++ dj_rest_tickets/utils.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class CustomPageNumberPagination(pagination.PageNumberPagination):
     page_query_param = 'page'
     page_size_query_param = 'page_size'
-    page_size = 10
+    page_size = 20
     max_page_size = 100
 
     def get_paginated_response(self, data):
